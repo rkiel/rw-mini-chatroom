48148d4d6087a071356dd86dde055a65a9333b07
rkiel <cygnus@pobox.com>
2015-03-10 11:50:38 -0600

    updated ChatList

--- app/ChatList.js
+++ app/ChatList.js
@@ -1,13 +1,76 @@
 var React = require('react');
 var $ = require('jquery');
 
-var ChatList = React.createClass({
-  render: function(){
-    return (
-      <ul className="list-group">
-      </ul>
-    )
+function getInitialState() {
+  return {
+    chats: []
   }
+}
+
+function propTypes() {
+  return {
+    url: React.PropTypes.string.isRequired
+  };
+}
+
+function getDefaultProps() {
+  return {
+    url: 'https://api.parse.com/1/classes/chat'
+  };
+}
+
+
+function getChats() {
+  $.ajax({
+    url: this.props.url,
+    type: 'GET',
+    beforeSend: function(request) {
+      request.setRequestHeader("X-Parse-Application-Id", 'YOUR-PARSE-APP-ID');
+      request.setRequestHeader("X-Parse-REST-API-Key", 'YOUR-PARSE-API-KEY');
+      request.setRequestHeader("Content-Type", 'application/json');
+    },
+    error: function(data) {
+      console.log('There was an error in getting the chats');
+    },
+    success: function(data) {
+      if (this.isMounted()) {
+        this.setState({
+          chats: data.results
+        });
+      }
+    }.bind(this)
+  })
+}
+
+function render(){
+  var list = this.state.chats.map(function(item, index){
+       return <li className="list-group-item" key={item.objectId}> {item.text} </li>
+  });
+  return (
+    <ul className="list-group">
+      {list}
+    </ul>
+  )
+}
+
+function componentDidMount() {
+  this.interval = setInterval(function(){
+    this.getChats();
+  }.bind(this), 1000)
+}
+
+function componentWillUnmount() {
+  clearInterval(this.interval);
+}
+
+var ChatList = React.createClass({
+  getInitialState:      getInitialState,
+  propTypes:            propTypes(),
+  getDefaultProps:      getDefaultProps,
+  componentDidMount:    componentDidMount,
+  componentWillUnmount: componentWillUnmount,
+  getChats:             getChats,
+  render:               render
 });
 
 module.exports = ChatList;
